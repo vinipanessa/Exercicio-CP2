2ad1153921442921fb115e06735ef900d3a19044
vinipanessa <vini.panessa@gmail.com>
2024-10-03 22:27:02 -0300

    colocando o codigo completo

---
+++ app/src/main/java/com/example/exercicio_cp2/model/TicketResponse.kt
@@ -0,0 +1,15 @@
+package com.example.exercicio_cp2.model
+
+class TickerResponse(
+    val ticker: Ticker
+)
+
+class Ticker(
+    val high: String,
+    val low: String,
+    val vol: String,
+    val last: String,
+    val buy: String,
+    val sell: String,
+    val date: Long
+)
